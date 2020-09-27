529f348ab9fe49a737ebca6def66921765aa85fa
Rebecca <rebecca_517@hotmail.com>
2020-09-27 01:17:55 -0400

    fetch movies in grid view

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class MovieGridViewController: UIViewController {
+    
+    var movies = [[String: Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("view did load")
 
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        print(request)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+          // This will run when the network request returns
+          if let error = error {
+             print(error.localizedDescription)
+          } else if let data = data {
+            print("data")
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            self.movies = dataDictionary["results"] as! [[String : Any]]
+            print(self.movies)
+
+          }
+       }
+        task.resume()
     }
     
 
